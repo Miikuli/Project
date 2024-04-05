d68fd9263f5d6c24260c6822e9accf7e6f5ec806
rdstnwmn <an.antipova.05@mail.ru>
2024-04-05 18:57:01 +0500

    second

--- ourapp/users/views.py
+++ ourapp/users/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
+
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Ваш аккаунт создан: можете войти на сайт. {username}!')
+            messages.success(request, f'Ваш аккаунт создан: можете войти на сайт!')
             return redirect('login')
     else:
         form = UserRegisterForm()
